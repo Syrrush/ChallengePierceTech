10847d0914fe92aed19c578b11bcc270017db8eb
Syrrush <zanattasebas@gmail.com>
2023-12-13 21:20:04 -0300

    Making the structure

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banners from "../common/Banners";
+import BestSellers from "../common/BestSellers";
+import Categories from "../common/Categories";
+import Footer from "../common/Footer";
+import NavBar from "../common/NavBar";
+import PaymentOptions from "../common/PaymentOptions";
+import Trends from "../common/Trends";
+
+export default function Home({ }) {
+    <>
+        <NavBar />
+        <Banners />
+        <Trends />
+        <Categories />
+        <BestSellers />
+        <PaymentOptions />
+        <Footer />
+    </>
+}
